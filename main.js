adf09c2b1434b2999f2f63b55e1ad8c11e4f3d95
saan1984 <sandeep_giet@yahoo.com>
2015-08-15 23:10:08 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var application = require('app'),
+    BrowserWindow = require('browser-window');
+
+application.on('ready', function() {
+    var mainWindow = new BrowserWindow({
+        width: 700,
+        height: 400,
+        center:true,
+        title:"ElectronPowerMonitorDemo",
+    });
+    mainWindow.loadUrl('file://' + __dirname + '/main.html');
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
